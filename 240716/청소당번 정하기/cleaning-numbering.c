de10553caf06b8456d68136b65caf321717fb35e
SXXNWN <julia020115@naver.com>
2024-07-16 19:31:16 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240716/청소당번 정하기/cleaning-numbering.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    
+    int cnt1=0;
+    int cnt2=0;
+    int cnt3=0;
+    int n;
+
+    scanf("%d" , &n);
+
+    for(int i=1;i<=11;i++){
+        if(i%2==0 && i%3==0 && i%12==0){
+            cnt3++;
+        }
+        else if(i%2==0 && i%3==0){
+            cnt2++;
+        }
+        else if(i%3==0 && i%12==0){
+            cnt3++;
+        }
+        else if(i%2==0 && i%12==0){
+            cnt3++;
+        }
+        else if(i%2==0){
+            cnt1++;
+        }
+        else if(i%3==0){
+            cnt2++;
+        }
+        else if(i%12==0){
+            cnt3++;
+        }
+    }
+
+    printf("%d %d %d" , cnt1 , cnt2 , cnt3);
+
+    return 0;
+}
