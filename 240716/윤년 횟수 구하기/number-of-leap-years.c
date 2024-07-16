a2bdf1d2291b315ae446d6a2ddeefaba7421796e
SXXNWN <julia020115@naver.com>
2024-07-16 23:16:52 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/윤년 횟수 구하기/number-of-leap-years.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n , cnt=0;
+
+    scanf("%d" , &n);
+
+    for(int i=1;i<=n;i++){
+
+        if(i%100==0 && i%400!=0 ){
+            continue;
+        }
+        else if(i%4==0){
+            cnt++;
+        }
+    }
+
+    printf("%d" , cnt);
+    return 0;
+}
