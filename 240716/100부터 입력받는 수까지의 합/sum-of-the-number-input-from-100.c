a876bf934aa1a5d66c49d1a129325fff39e710ca
SXXNWN <julia020115@naver.com>
2024-07-16 23:37:15 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n , sum=0;
+
+    scanf("%d" , &n);
+
+    for(int i=n;i<=100;i++){
+        sum+=i;
+    }
+
+    printf("%d" , sum);
+    
+    return 0;
+}
