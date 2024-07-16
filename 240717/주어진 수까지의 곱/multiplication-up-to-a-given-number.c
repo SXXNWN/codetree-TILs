37f45c2cb4b8b0e4d3ac43fd216ed955f2e1e9fe
SXXNWN <julia020115@naver.com>
2024-07-17 00:41:06 +0900

    Title: 주어진 수까지의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/주어진 수까지의 곱/multiplication-up-to-a-given-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a , b , prod = 1;
+
+    scanf("%d %d" , &a , &b);
+
+    for(int i = a ; i <=b ; i++){
+        prod *= i;
+    }
+    printf("%d" , prod);
+
+    return 0;
+}
