657aa45e72cb295523ffc267c48c969da30e1bb1
SXXNWN <julia020115@naver.com>
2024-07-17 00:31:52 +0900

    Title: 입력받은 수의 합과 평균, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n , sum = 0;
+
+    scanf("%d" , &n);
+
+    for(int i = 0 ; i < n ; i++){
+        int a;
+        scanf("%d" , &a);
+        sum += a;
+        
+    }
+
+    printf("%d %.1lf" , sum , (double)sum/n);
+
+    return 0;
+}
