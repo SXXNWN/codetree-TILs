5c62430ecc359e5061a13ae700a860abf47dfd85
SXXNWN <julia020115@naver.com>
2024-07-16 23:45:14 +0900

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/홀수만 더하기/add-only-odd-numbers.c
+++ 240716/홀수만 더하기/add-only-odd-numbers.c
@@ -9,11 +9,12 @@ int main() {
     for(int i=1;i<=n;i++){
         int x;
         scanf("%d" , &x);
-        if(x%2!=0 && x%3==0){
+        if(x%2==1 && x%3==0){
             sum+=x;
         }
     
-    printf("%d" , sum);
+    
     }
+    printf("%d" , sum);
     return 0;
 }
