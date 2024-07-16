a6f280458b98b4c21248e38373dc09f5621020a1
SXXNWN <julia020115@naver.com>
2024-07-16 23:44:08 +0900

    Title: 홀수만 더하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240716/홀수만 더하기/add-only-odd-numbers.c
+++ 240716/홀수만 더하기/add-only-odd-numbers.c
@@ -4,11 +4,11 @@ int main() {
     
     int n,sum=0;
 
-    scanf("%d " , &n);
+    scanf("%d" , &n);
 
     for(int i=0;i<n;i++){
         int x;
-        scanf("%d " , &x);
+        scanf("%d" , &x);
         if(x%2!=0 && x%3==0){
             sum+=x;
         }
