2c5aafbd80efad43233c3a849941353c9495e063
SXXNWN <julia020115@naver.com>
2024-07-17 14:07:38 +0900

    Title: 출석 부르기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/출석 부르기 2/calling-attendance-2.c
+++ 240717/출석 부르기 2/calling-attendance-2.c
@@ -7,7 +7,7 @@ int main() {
 
         scanf("%d" , &n);
 
-        if(n==1)printf("Jonh\n");
+        if(n==1)printf("John\n");
         else if(n==2)printf("Tom\n");
         else if(n==3)printf("Paul\n");
         else if(n==4)printf("Sam\n");
