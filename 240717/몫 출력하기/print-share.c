2034304b4591f85b8e0c0c0da67d8d83e970de88
SXXNWN <julia020115@naver.com>
2024-07-17 14:06:32 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/몫 출력하기/print-share.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int cnt = 0;
+
+    while(1){
+
+        int n;
+        scanf("%d" , &n);
+
+        if(n%2==1)
+            continue;
+        else{
+            printf("%d\n" , n/2);
+            cnt++;
+            if(cnt==3) 
+                break;
+        }
+    }
+    return 0;
+}
