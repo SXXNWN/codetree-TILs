1f6cfcc5115a085f5e4bec4e6a2519fbb87d9128
SXXNWN <julia020115@naver.com>
2024-07-17 13:17:36 +0900

    Title: 1부터 곱셈하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/1부터 곱셈하기/multiple-from-one.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n,prod=1;
+
+    scanf("%d" , &n);
+
+    for(int i=1 ; i <=10 ; i++){
+        
+        prod*=i;
+
+        if(prod>=n){
+            printf("%d" , i);
+            break;
+        }
+    }
+    return 0;
+}
