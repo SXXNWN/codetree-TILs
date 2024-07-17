e96794393a942c8efbe9b7065e2e46edff6ec2d9
SXXNWN <julia020115@naver.com>
2024-07-17 14:17:21 +0900

    Title: 20대의 평균나이, Time: 1028ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240717/20대의 평균나이/average-age-of-20.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+    int cnt = 0 , sum = 0;
+
+    while(1){
+        int n ;
+
+        scanf("%d" , &n);
+
+        if(n<30){
+        sum += n;
+        cnt++;
+        }
+
+        else
+            break;
+    }
+
+    printf("%.2lf" , (double)sum/cnt);
+    return 0;
+}
