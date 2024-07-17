7e8fbb7e1fbf83188a7c64347bcd4b41b680ccd0
SXXNWN <julia020115@naver.com>
2024-07-17 15:14:28 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/조건에 따른 연산/operatino-by-rule.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n , cnt=0;
+
+    scanf("%d" , &n);
+
+    while(1){
+    if(n%2==0){
+        n = n*3 + 1;
+        cnt++;
+    }
+    else{
+        n = n*2 + 2;
+        cnt++;
+    }
+
+    if(n>=1000){
+        printf("%d" , cnt);
+        break;
+    }
+    }
+    return 0;
+}
