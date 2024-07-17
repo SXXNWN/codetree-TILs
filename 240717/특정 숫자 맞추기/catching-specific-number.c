463a9225f18e0675f87926a494fa1e804168a0a7
SXXNWN <julia020115@naver.com>
2024-07-17 13:51:26 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/특정 숫자 맞추기/catching-specific-number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    while(1){
+        scanf("%d" , &n);
+
+        if(n<25){
+            printf("Higher\n");
+        }
+        else if(n>25){
+            printf("Lower\n");
+        }
+        else{
+            printf("Good\n");
+            break;
+        }
+    }
+    return 0;
+}
