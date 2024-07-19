c510a86092496528576456557ae6b31bb2165424
SXXNWN <julia020115@naver.com>
2024-07-19 15:58:04 +0900

    Title: 별 그리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/별 그리기/star-drawing.c
+++ 240719/별 그리기/star-drawing.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d" , &n);
 
     for(int i = 0 ; i < n ; i++){
-        for(int j = n ; j > i ; j--){
+        for(int j = 0 ; j < n - (i + 1) ; j++){
             printf(" ");
         }
         for(int j = 0 ; j < i*2 + 1 ; j ++){
