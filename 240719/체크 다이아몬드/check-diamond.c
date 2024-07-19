314ed35179d7aad96ddb015e1f8a7f4b9f2f796e
SXXNWN <julia020115@naver.com>
2024-07-19 17:00:05 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/체크 다이아몬드/check-diamond.c
+++ 240719/체크 다이아몬드/check-diamond.c
@@ -15,11 +15,11 @@ int main() {
         printf("\n");
     }
 
-    for(int i = 0 ; i < n - 1 ; i++){
-        for(int j = 1 ; j < i + n-1 ; j++){
+    for(int i = 1 ; i < n + 1 ; i++){
+        for(int j = 0 ; j < i  ; j++){
             printf(" ");
         }
-        for(int j = n ; j > i+1 ; j--){
+        for(int j = n ; j > i ; j--){
             printf("* ");
         }
         printf("\n");
