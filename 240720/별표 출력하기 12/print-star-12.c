86c9efb70af5c89723c5b18265c668c0680689ac
SXXNWN <julia020115@naver.com>
2024-07-20 14:52:14 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240720/별표 출력하기 12/print-star-12.c
+++ 240720/별표 출력하기 12/print-star-12.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
 int main() {
+    // 변수 선언
     int n;
-    scanf("%d" , &n);
-
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < n ; j++){
-            if(i==0)printf("* ");
-            else{
-                if(i%2==0 && i==j) printf("* ");
+    
+    // 입력
+    scanf("%d", &n);
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(j % 2 == 0) {
+                if(i == 0) 
+                    printf("* ");
+                else
+                    printf("  ");
+            }
+            else {
+                if(i <= j)
+                    printf("* ");
+                else
+                    printf("  ");
             }
         }
         printf("\n");
