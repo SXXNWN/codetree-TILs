d43171bdf43c8f1b3f0ec5075f143d686f305dd2
SXXNWN <julia020115@naver.com>
2025-09-16 19:31:32 +0900

    Title: 수의 계절은, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250916/수의 계절은/season-of-num.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+if 3 <= N and N <= 5 :
+    print('Spring')
+elif 6 <= N and N <= 8 :
+    print('Summer')
+elif 9 <= N and N <= 11 :
+    print('Fall')
+else :
+    print('Winter')
