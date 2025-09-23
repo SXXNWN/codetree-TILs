957397502e3d29c80add5d74ea5b32a9844388cd
SXXNWN <julia020115@naver.com>
2025-09-23 20:52:01 +0900

    Title: A/B 출력, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250923/A/B 출력/a-divide-b.py
@@ -0,0 +1,16 @@
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+print(f"{a//b}." , end = "")
+
+a %= b
+
+for i in range(20):
+    
+    a *= 10
+    print(a // b, end="")
+
+    
+    a %= b
