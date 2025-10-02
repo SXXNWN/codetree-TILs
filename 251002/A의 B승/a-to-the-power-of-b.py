8b5f38e83193f258953bda5722aee08449aa636a
SXXNWN <julia020115@naver.com>
2025-10-02 14:06:06 +0900

    Title: A의 B승, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251002/A의 B승/a-to-the-power-of-b.py
+++ 251002/A의 B승/a-to-the-power-of-b.py
@@ -1,7 +1,13 @@
 A , B = map(int,input().split())
 C = A
+D = 1
 
-for i in range(B-1):
-    A *= C
+for i in range(B):
+    if i == 0 :
+        A *= D
+    else :
+        A *= C
+    
+   
 
 print(A)
