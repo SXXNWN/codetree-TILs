f616216919b3e8fd49f95febb99a27f1284f45b3
SXXNWN <julia020115@naver.com>
2025-11-02 19:42:27 +0900

    Title: 조건에 따른 연산, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251102/조건에 따른 연산/operatino-by-rule.py
@@ -0,0 +1,15 @@
+cnt = 0
+N = int(input())
+
+while(True):
+    if N >= 1000 :
+        print(cnt)
+        break
+    
+    if N % 2 == 0 :
+        N = N * 3 + 1
+        cnt += 1
+    else :
+        N = N * 2 + 1
+        cnt += 1
+
