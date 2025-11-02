a35c39d4832098594cd66f7e1b84f8b11a58190c
SXXNWN <julia020115@naver.com>
2025-11-02 18:54:44 +0900

    Title: 몫 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251102/몫 출력하기/print-share.py
@@ -0,0 +1,13 @@
+cnt = 0
+
+while(True):
+    N = int(input())
+
+    if(N % 2 != 0):
+        continue
+    else :
+        print(int(N/2))
+        cnt += 1
+    
+    if cnt == 3 :
+        break
