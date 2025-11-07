2fe0ec4aa71cb1f90a4c21b78c26a5d6a25e0bda
SXXNWN <julia020115@naver.com>
2025-11-07 19:11:05 +0900

    Title: 9-1 정사각형, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251107/9-1 정사각형/9-1-square.py
@@ -0,0 +1,13 @@
+n = int(input())
+cnt = 9
+
+for i in range(n):
+    for j in range(n):
+        if cnt == 0 :
+            cnt = 9
+            print(cnt , end = "")
+            cnt -= 1
+        else :
+            print(cnt , end = "")
+            cnt -= 1
+    print()
