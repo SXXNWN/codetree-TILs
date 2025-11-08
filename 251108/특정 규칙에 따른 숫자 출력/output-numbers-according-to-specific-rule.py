8446b66f481acbe184e61cacee5014eee6954f31
SXXNWN <julia020115@naver.com>
2025-11-08 16:05:43 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251108/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -0,0 +1,15 @@
+n = int(input())
+cnt = 1
+
+for i in range(n , 0 , -1):
+    for j in range(n-i):
+        print(" " , end = " ")
+    for j in range(i):
+        if cnt == 10 :
+            cnt = 1
+            print(cnt , end = " ")
+            cnt += 1
+        else :
+            print(cnt , end = " ")
+            cnt += 1
+    print()
