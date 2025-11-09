b032370d647df282cb648f1d94620a656537ca41
SXXNWN <julia020115@naver.com>
2025-11-09 13:43:49 +0900

    Title: 완전수, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251109/완전수/perfect-number.py
@@ -0,0 +1,14 @@
+start, end = map(int, input().split())
+
+# Please write your code here.
+sum = 0
+cnt = 0
+
+for i in range(start , end + 1):
+    for j in range(1 , i):
+        if i % j == 0 :
+            sum += j
+    if sum == i :
+        cnt += 1
+
+print(cnt)
