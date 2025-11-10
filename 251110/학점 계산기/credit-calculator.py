b8dad1fb08adf095a35b354515fc4af716f31481
SXXNWN <julia020115@naver.com>
2025-11-10 15:12:16 +0900

    Title: 학점 계산기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251110/학점 계산기/credit-calculator.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = list(map(float , input().split()))
+sum_val = 0
+
+for i in range(n):
+    sum_val += arr[i]
+
+avg_val = sum_val / n
+
+print(f"{avg_val:.1f}")
+
+if avg_val >= 4.0 :
+    print("Perfect")
+elif avg_val >= 3.0 :
+    print("Good")
+else :
+    print("Poor")
