d759f0e50627dda5f472814ae189d4295c704a09
SXXNWN <julia020115@naver.com>
2025-11-13 14:22:38 +0900

    Title: 점수대 파악하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251113/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,14 @@
+arr = list(map(int , input().split()))
+set_val = 0
+
+for i in range(len(arr)):
+    if arr[i] == 0 :
+        set_val = i
+        break
+
+for i in range(100 , 0 , -10):
+    cnt = 0
+    for j in range(set_val):
+        if arr[j] // 10 == i // 10 :
+            cnt += 1
+    print(f"{i} - {cnt}")
