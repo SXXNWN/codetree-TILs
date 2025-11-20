f1b492effca078d32edbfaf2b478e6096285aa1b
SXXNWN <julia020115@naver.com>
2025-11-21 05:29:22 +0900

    Title: N개의 정수 중 최대 2개, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251121/N개의 정수 중 최대 2개/two-max-of-n-num.py
+++ 251121/N개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -17,11 +17,14 @@ for i in range(n):
         check = i
         break
 
+max_val_2_check = False
 
 for i in range(n):
     if i == check :
         continue
     
+    if not max_val_2 :
+        max_val_2_check , max_val_2 = True , a[i] 
     if max_val_2 <= a[i]:
         max_val_2 = a[i]
 
