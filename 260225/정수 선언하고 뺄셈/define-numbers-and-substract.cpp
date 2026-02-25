b8b17897d7ea40118e2ff8e260f44602ee6d78cc
SXXNWN <julia020115@naver.com>
2026-02-25 16:05:56 +0900

    Title: 정수 선언하고 뺄셈, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260225/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
+++ 260225/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int a = 97 , b = 13
+    int a = 97 , b = 13;
     cout << a << " - " << b << " = " << a-b;
     return 0;
 }
