89ccd3bd8f91e8f24c2c0e93ac60d672f8bf8aad
SXXNWN <julia020115@naver.com>
2026-02-26 15:13:24 +0900

    Title: 세 정수형 변수 선언, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260226/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
+++ 260226/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int a , b , c = 7 , 23 , 30;
+    int a = 7 , b = 23 , c = 30;
 
     cout << a << " + " << b << " = " << c;
     return 0;
