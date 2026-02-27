e6c3c680718eddbead47178b8181a2589fcd0130
SXXNWN <julia020115@naver.com>
2026-02-27 16:25:17 +0900

    Title: 길이 단위 변환하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260227/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+
+    double a = 9.2 , b = 1.3;
+
+    cout.precision(1);
+
+    cout << a << "ft" << " = " << a*30.48 << "cm";
+    cout << b << "mi" << " = " << b*160934 << "cm";
+
+    
+    return 0;
+}
